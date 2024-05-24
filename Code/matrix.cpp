57f20192a2a40e03594667faa9fda24666e23f9d
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-24 23:12:52 +0200

    Update matrix.cpp

--- Code/matrix.cpp
+++ Code/matrix.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
 
+#include "matrix.h"
+using namespace std;
+
+//Do not change the displayMatrix() function
+void Matrix::Matrix::displayMatrix() {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+
+            cout << *matrix[i][j];
+
+            if (j != columns - 1) {
+                cout << " ";
+            }
+        }
+
+        cout << endl;
+    }
+}
