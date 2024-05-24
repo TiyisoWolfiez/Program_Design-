ad9d8f13220090569602dbaadd2337a7e67a7a9b
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-24 23:13:17 +0200

    Update matrix.cpp

--- Code/matrix.cpp
+++ Code/matrix.cpp
@@ -18,3 +18,52 @@ void Matrix::Matrix::displayMatrix() {
         cout << endl;
     }
 }
+Matrix::Matrix(int in_rows, int in_columns){
+    if(in_rows<1 || in_columns<1){
+        this->columns = 3;
+        this->rows = 3;
+        int **arrTemp = new int*[this->rows];
+        for(int k_iterator = 0;k_iterator<this->rows;k_iterator++){
+            arrTemp[k_iterator] = new int[this->columns];
+        }
+        for(int k_iterator = 0;k_iterator<this->rows;k_iterator++){
+            for(int t_iterator = 0;t_iterator<this->columns;t_iterator++){
+                arrTemp[k_iterator][t_iterator] = 1;
+            }
+        }
+        this->matrix = new int**[this->rows];
+        for(int k_iterator = 0;k_iterator<this->rows;k_iterator++){
+            this->matrix[k_iterator] = new int*[this->columns];
+        }
+        int index = 0;
+        for(int k_iterator = 0;k_iterator<this->rows;k_iterator++){
+            for(int t_iterator = 0;t_iterator<this->columns;t_iterator++){
+                this->matrix[k_iterator][t_iterator] = &arrTemp[k_iterator][t_iterator];
+            }
+        }
+    }
+    else{
+        int temp2 = 0;
+        this->columns = in_columns;
+        this->rows = in_rows;
+        int **arrTemp = new int*[this->rows];
+        for(int k_iterator = 0;k_iterator<this->rows;k_iterator++){
+            arrTemp[k_iterator] = new int[this->columns];
+        }
+        for(int k_iterator = 0;k_iterator<this->rows;k_iterator++){
+            for(int t_iterator = 0;t_iterator<this->columns;t_iterator++){
+                arrTemp[k_iterator][t_iterator] = 0;
+            }
+        }
+        this->matrix = new int**[this->rows];
+        for(int k_iterator = 0;k_iterator<this->rows;k_iterator++){
+            this->matrix[k_iterator] = new int*[this->columns];
+        }
+        int index = 0;
+        for(int k_iterator = 0;k_iterator<this->rows;k_iterator++){
+            for(int t_iterator = 0;t_iterator<this->columns;t_iterator++){
+                this->matrix[k_iterator][t_iterator] = &arrTemp[k_iterator][t_iterator];
+            }
+        }
+    }
+}
