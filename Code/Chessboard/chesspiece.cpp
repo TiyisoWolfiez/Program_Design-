1fbef9d82888c495ccf126f8fd4db6a02accbfbf
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-25 22:43:29 +0200

    Update chesspiece.cpp

--- Code/Chessboard/chesspiece.cpp
+++ Code/Chessboard/chesspiece.cpp
@@ -2,3 +2,9 @@
 
 // Instantiating the static Member Variable
 int Chesspiece::numberOfPieces = 0;
+
+Chesspiece::Chesspiece(){
+    this->numberOfPieces++;
+    this->name  = "";
+    this->type = true;
+}
