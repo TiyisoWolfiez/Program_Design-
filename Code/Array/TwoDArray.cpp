bc36e4d6a4bae2fa8bc11e6cf7672a0ebf9f4e9f
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-26 22:41:49 +0200

    Update TwoDArray.cpp

--- Code/Array/TwoDArray.cpp
+++ Code/Array/TwoDArray.cpp
@@ -1,1 +1,18 @@
+#include "TwoDArray.h"
 
+TwoDArray::TwoDArray(int** array, int* sizes, int baseSize){
+    this->baseSize = baseSize;
+    const int size = this->getBaseSize();
+    this->sizes = new int[size];
+    for(int i = 0;i<size;i++){
+        this->sizes[i] = sizes[i];
+    }
+    this->array = new int*[size];
+    for(int i = 0;i<size;i++){
+        const int size = this->getSizes()[i];
+        this->array[i] = new int[size];
+        for(int j = 0;j<size;j++){
+            this->array[i][j] = array[i][j];
+        }
+    }
+}
