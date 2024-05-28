9c74b287b139f7f32f5b248e32997bf054669058
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-28 22:30:11 +0200

    Update IsGreater.cpp

--- Code/Numbers/IsGreater.cpp
+++ Code/Numbers/IsGreater.cpp
@@ -4,3 +4,10 @@ int IsGreater::numAliveObjects = 0;
 IsGreater::IsGreater(int Value): ValueDependantTester(Value){
     this->numAliveObjects+=1;
 }
+IsGreater::~IsGreater(){
+    this->numAliveObjects-=1;
+}
+bool IsGreater::evaluate(int val){
+    bool Temp = (val>this->value);
+    return Temp;
+}
