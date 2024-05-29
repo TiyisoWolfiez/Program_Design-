e1e5827e5dc9d0dd1b9eb54790907c22cb520367
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-29 22:11:45 +0200

    Update TesterInterface.cpp

--- Code/Numbers/TesterInterface.cpp
+++ Code/Numbers/TesterInterface.cpp
@@ -1,1 +1,16 @@
-
+#include "TesterInterface.h"
+#include <iostream>
+TesterInterface::TesterInterface(int maxNumTests)
+{
+    this->maxNumTesters = this->currNumTesters = 0;
+    this->testers = new NumberTester*[0];
+    if(maxNumTests>=1)
+    {
+        this->maxNumTesters = maxNumTests;
+        this->testers = new NumberTester*[maxNumTests];
+        for(int i=0; i<maxNumTests; i+=1)
+        {
+            this->testers[i] = NULL;
+        }
+    }
+}
