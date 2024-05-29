bae7b291ae3d71e1781e661d5156d321f0de2651
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-29 22:10:26 +0200

    Update IsDivisible.cpp

--- Code/Numbers/IsDivisible.cpp
+++ Code/Numbers/IsDivisible.cpp
@@ -1,2 +1,8 @@
 #include "IsDivisible.h"
 int IsDivisible::numAliveObjects = 0;
+IsDivisible::IsDivisible(int Value): ValueDependantTester(Value){
+    this->numAliveObjects+=1;
+}
+IsDivisible::~IsDivisible(){
+    this->numAliveObjects-=1;
+}
