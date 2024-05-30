94f17152b24c4302356b0ba17b64f551f07043f2
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-30 22:31:12 +0200

    Update IsSmaller.cpp

--- Code/Numbers/IsSmaller.cpp
+++ Code/Numbers/IsSmaller.cpp
@@ -4,3 +4,10 @@ int IsSmaller::numAliveObjects = 0;
 IsSmaller::IsSmaller(int Value): ValueDependantTester(Value){
     this->numAliveObjects+=1;
 }
+IsSmaller::~IsSmaller(){
+    this->numAliveObjects-=1;
+}
+bool IsSmaller::evaluate(int val){
+    bool Temp = val<this->value;
+    return Temp;
+}
