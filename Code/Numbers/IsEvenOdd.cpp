2a4efd77ef3dcc8506096855d6febbbfaf7a5a3c
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-30 22:33:39 +0200

    Update IsEvenOdd.cpp

--- Code/Numbers/IsEvenOdd.cpp
+++ Code/Numbers/IsEvenOdd.cpp
@@ -4,3 +4,10 @@ int IsEvenOdd::numAliveObjects = 0;
 IsEvenOdd::IsEvenOdd(){
     this->numAliveObjects+=1;
 }
+IsEvenOdd::~IsEvenOdd(){
+    this->numAliveObjects-=1;
+}
+bool IsEvenOdd::evaluate(int val){
+    bool Temp = (val%2 == 0);
+    return Temp;
+}
