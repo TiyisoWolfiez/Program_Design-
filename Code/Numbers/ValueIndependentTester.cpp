6f8231bef78dbcf114c00cb63c662f9cd66bcdd4
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-05-30 22:38:26 +0200

    Update ValueIndependentTester.cpp

--- Code/Numbers/ValueIndependentTester.cpp
+++ Code/Numbers/ValueIndependentTester.cpp
@@ -11,3 +11,10 @@ bool ValueIndependantTester::evaluate(int val){
     // This is a pure virtual function.
     return true;
 }
+NumberTester* ValueIndependantTester::clone(){
+    // This is a pure virtual function.
+    return NULL;
+}
+int ValueIndependantTester::getNumAliveObjects(){
+    return numAliveObjects;
+}
