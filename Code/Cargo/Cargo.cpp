4b712408761bc6e59806ed36f0f556e4afeb4d8b
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-01 23:38:50 +0200

    Update Cargo.cpp

--- Code/Cargo/Cargo.cpp
+++ Code/Cargo/Cargo.cpp
@@ -2,3 +2,16 @@
 #define Cargo_H
 #include <sstream>
 #include <string>
+
+class Cargo
+{
+    private:
+        double weight;
+        double value;
+    public:
+        Cargo(double weight, double value);
+        double getWeight() const;
+        double getValue() const;
+        std::string describe();
+};
+#endif
