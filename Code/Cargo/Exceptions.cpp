4a12b9c5f32b2137a61997b2708d54f36b227cf0
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-02 23:48:04 +0200

    Update Exceptions.cpp

--- Code/Cargo/Exceptions.cpp
+++ Code/Cargo/Exceptions.cpp
@@ -5,3 +5,11 @@ using namespace std;
 AboveWeightEx::AboveWeightEx(double weight){
     AboveWeightEx::weightDiff = weight;
 }
+double AboveWeightEx::getDifference() const{
+    return AboveWeightEx::weightDiff;
+}
+
+template <class E>
+NotFoundEx<E>::NotFoundEx(E* miss){
+    NotFoundEx<E>::missing = miss;
+}
