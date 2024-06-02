6c7ac385f0cb987a33a0384b954a11d8adc142b4
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-02 23:46:25 +0200

    Update Exceptions.h

--- Code/Cargo/Exceptions.h
+++ Code/Cargo/Exceptions.h
@@ -1,3 +1,14 @@
 #ifndef Exception_H
 #define Exception_H
 #include <sstream>
+
+template <class E>
+class UndervaluedEx{
+    private:
+        E* underVal;
+        double sum;
+    public:
+        UndervaluedEx(E* UnderVal1, double sum);
+        E* getUndervalued() const;
+        double getSum() const;
+};
