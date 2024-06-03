b09647998ba57f88adf12fc5dd9e0997f6b3b83f
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-03 13:10:21 +0200

    Update Plane.cpp

--- Code/Cargo/Plane.cpp
+++ Code/Cargo/Plane.cpp
@@ -6,3 +6,24 @@ template <class T>
 Plane<T>::Plane(double maxWeight){
     Plane<T>::maxWeight = maxWeight;
 }
+template <class T>
+void Plane<T>::remove(T* var){
+    bool found = false;
+    int index = -1*Plane<T>::cargo.size();
+    if(Plane<T>::cargo.empty() == !false){
+        throw EmptyEx();
+    }
+    for(int i=0, n = Plane<T>::cargo.size(); i< n; i++){
+        if(Plane<T>::cargo[i]==var){
+            index = i;
+            found = true;
+            break;
+        }
+    }
+    if(found == false){
+        throw NotFoundEx<T>(var);
+    }
+    else{
+        Plane<T>::cargo.erase(Plane<T>::cargo.begin() + index);
+    }
+}
