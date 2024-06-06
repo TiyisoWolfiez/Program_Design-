18f26fac64070d73daeb0870f121ad4ff15c5452
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-06 20:10:41 +0200

    Update Passenger.h

--- Code/Cargo/Passenger.h
+++ Code/Cargo/Passenger.h
@@ -2,3 +2,17 @@
 #define Passenger_H
 #include <sstream>
 #include <string>
+
+class Passenger
+{
+    private:
+        std::string ID;
+        double weight;
+        double ticketPrice;
+    public:
+        Passenger(std::string ID, double weight, double ticketPrice);
+        double getWeight() const;
+        double getValue() const;
+        std::string describe();
+};
+#endif
