ff2ed73b142a37b9508f9413972c214ed0973967
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-06 20:11:22 +0200

    Update Passenger.cpp

--- Code/Cargo/Passenger.cpp
+++ Code/Cargo/Passenger.cpp
@@ -1,3 +1,9 @@
 //The Passenger implementation file
 #include "Passenger.h"
 using namespace std;
+
+Passenger::Passenger(string ID, double weight, double ticketPrice){
+    Passenger::ID = ID;;
+    Passenger::weight = weight;
+    Passenger::ticketPrice = ticketPrice;
+}
