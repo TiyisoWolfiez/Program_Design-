5fbf6d968e75a7e48d995870c2b642404d15ed5a
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-06 20:11:41 +0200

    Update Passenger.cpp

--- Code/Cargo/Passenger.cpp
+++ Code/Cargo/Passenger.cpp
@@ -7,3 +7,9 @@ Passenger::Passenger(string ID, double weight, double ticketPrice){
     Passenger::weight = weight;
     Passenger::ticketPrice = ticketPrice;
 }
+double Passenger::getWeight() const{
+    return Passenger::weight;
+}
+double Passenger::getValue() const{
+    return Passenger::ticketPrice;
+}
