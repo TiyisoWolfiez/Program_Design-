0a5423b4069808b778aefc490b2e585a1299223d
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-08 22:11:43 +0200

    Update SortList.cpp

--- Code/Sort/SortList.cpp
+++ Code/Sort/SortList.cpp
@@ -1,1 +1,21 @@
+#include "SortList.h"
+#include <iostream>
+using namespace std;
 
+template<class T>
+SortList<T>::SortList(bool val){
+    this->ascending = val;
+    this->tail = this->head = NULL;
+}
+template<class T>
+void SortList<T>::add(SortNode<T>* val){
+    if(this->tail != NULL){
+        val->prev = this->tail;
+        this->tail->next = val;
+        this->tail = val;
+    }
+    else{
+        this->tail = val;
+        this->head = val;
+    }
+}
