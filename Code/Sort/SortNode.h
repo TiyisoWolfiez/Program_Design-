34ac92cb707c45f3105c361103f41ce5da20a888
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-10 01:03:53 +0200

    Update SortNode.h

--- Code/Sort/SortNode.h
+++ Code/Sort/SortNode.h
@@ -8,3 +8,16 @@
 template<class T>
 class SortNode {
     public:
+// SortNode Pointers
+        SortNode<T>* next;
+        SortNode<T>* prev;
+        // SortNode Member functions
+        SortNode(T val);
+        std::string print();
+        T getValue();
+    protected:
+        T value;
+    private:
+};
+#include "SortNode.cpp"
+#endif
