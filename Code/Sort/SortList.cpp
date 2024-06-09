330dbe78e3c52ca6e136248a778c1288e1c0b227
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-09 22:02:45 +0200

    Update SortList.cpp

--- Code/Sort/SortList.cpp
+++ Code/Sort/SortList.cpp
@@ -42,3 +42,36 @@ std::string SortList<T>::print(){
     }
     return temp_string.substr(0, temp_string.length()-1);
 }
+template<class T>
+SortNode<T>* SortList<T>::getHead(){
+    return this->head;
+}
+template<class T>
+SortNode<T>* SortList<T>::remove(T val){
+    SortNode<T>* temp_node = this->head;
+    for( ; temp_node != NULL && temp_node->getValue() != val ; ){
+        temp_node = temp_node->next;
+    }
+    if(temp_node != NULL){
+        if(temp_node == this->head || temp_node == this->tail){
+            if(temp_node == this->head && temp_node == this->tail){
+                this->head = this->head->next;
+                this->tail = this->tail->next;
+            }
+            else if(temp_node == this->tail){
+                this->tail = this->tail->prev;
+                this->tail->next = temp_node->prev = NULL;
+            }
+            else{
+                this->head = this->head->next;
+                this->head->prev = temp_node->next = NULL;
+            }
+        }
+        else if(temp_node != this->head && temp_node != this->tail){
+            temp_node->prev->next = temp_node->next;
+            temp_node->next->prev = temp_node->prev;
+            temp_node->prev = temp_node->next = NULL;
+        }
+    }
+    return temp_node;
+}
