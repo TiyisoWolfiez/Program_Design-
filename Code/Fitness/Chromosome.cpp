736ba9ec905d8ba78406a598b150e7a7d8cb0dfb
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-13 23:31:34 +0200

    Update Chromosome.cpp

--- Code/Fitness/Chromosome.cpp
+++ Code/Fitness/Chromosome.cpp
@@ -1,1 +1,17 @@
+#include "Chromosome.h"
 
+Chromosome::Chromosome(int numGenes, RandomGenerator* rand){
+    if(numGenes<0){ this->numGenes = 0;}
+    else{ this->numGenes = numGenes;}
+    this->genes = new bool[this->numGenes];
+    if(rand == NULL){
+        for(int k_iterator=0;k_iterator<this->numGenes;k_iterator++){
+            genes[k_iterator]=false;
+        }
+    }
+    else{
+        for(int k_iterator=0;k_iterator<this->numGenes;k_iterator++){
+            genes[k_iterator]=rand->randomBool();
+        }
+    }
+}
