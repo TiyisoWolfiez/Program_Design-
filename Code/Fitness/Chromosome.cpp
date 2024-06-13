e960967442748eb2ad2d1e3719636ddded78bfea
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-13 23:34:20 +0200

    Update Chromosome.cpp

--- Code/Fitness/Chromosome.cpp
+++ Code/Fitness/Chromosome.cpp
@@ -30,3 +30,23 @@ Chromosome::Chromosome(Chromosome* chromosome){
         Chromosome(k_iterator, NULL);
     }
 }
+Chromosome::Chromosome(bool* genes, int numGenes){
+    if(numGenes<0){
+        this->numGenes = 0;
+        this->genes = new bool[0];
+    }
+    else if(genes == NULL && numGenes>=0){
+        this->numGenes = numGenes;
+        this->genes = new bool[this->numGenes];
+        for(int k_iterator=0;k_iterator<this->numGenes;k_iterator++){
+            this->genes[k_iterator]=false;
+        }
+    }
+    else{
+        this->numGenes = numGenes;
+        this->genes = new bool[this->numGenes];
+        for(int k_iterator=0;k_iterator<this->numGenes;k_iterator++){
+            this->genes[k_iterator]=genes[k_iterator];
+        }
+    }
+}
