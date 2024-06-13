b4cdd14c89f20daefea0b7b73a55d4cefd306557
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-13 23:29:25 +0200

    Update RandomGenerator.cpp

--- Code/Fitness/RandomGenerator.cpp
+++ Code/Fitness/RandomGenerator.cpp
@@ -4,3 +4,9 @@ RandomGenerator::RandomGenerator(int seed){
     srand(seed);
     this->seed = seed;
 }
+bool RandomGenerator::randomBool(){
+    if(rand()%2 == 0){
+        return false;
+    }
+    return true;
+}
