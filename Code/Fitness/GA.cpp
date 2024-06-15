6189af3ec0e3b23c8482353bd8e2315aea0b5388
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-15 22:26:05 +0200

    Update GA.cpp

--- Code/Fitness/GA.cpp
+++ Code/Fitness/GA.cpp
@@ -9,3 +9,17 @@ GA::GA(int populationSize, RandomGenerator* rand, int numGenes, int selectionSiz
         this->population[k_iterator]=new Chromosome(numGenes, rand);
     }
 }
+GA::GA(GA* geneticAlgorithm){
+    this->population=NULL;
+    this->populationSize = 0;
+    this->selectionSize = 0;
+    if(geneticAlgorithm!=NULL){
+        const int length_Array = geneticAlgorithm->populationSize;
+        this->populationSize = geneticAlgorithm->populationSize;
+        this->selectionSize = geneticAlgorithm->selectionSize;
+        this->population = new Chromosome*[length_Array];
+        for(int k_iterator=0;k_iterator<length_Array;k_iterator++){
+            this->population[k_iterator]=new Chromosome(geneticAlgorithm->population[k_iterator]);
+        }
+    }
+}
