642d21d519d1f7157bc90598816b270caad7bf4b
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-18 22:15:21 +0200

    Update vector.cpp

--- Code/Vectors/vector.cpp
+++ Code/Vectors/vector.cpp
@@ -1,2 +1,12 @@
 #include "matrix.h"
 #include <iomanip>
+
+Vector operator*(const double& multipler, const Vector& rhs)
+{
+	Vector obj(rhs.getSize());
+	for(int c=0;c<rhs.getSize();c++)
+	{
+		obj.vector[c]=multipler*rhs.vector[c];
+	}
+	return obj;
+}
