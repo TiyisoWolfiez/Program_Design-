cb587555ca9f25443d552c3b96c6f52fd2444f60
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-22 22:34:59 +0200

    Update matrix.cpp

--- Code/Vectors/matrix.cpp
+++ Code/Vectors/matrix.cpp
@@ -1,1 +1,16 @@
+#include "matrix.h"
+#include <iomanip>
+using namespace std;
 
+Matrix operator*(const double& multipler, const Matrix& rhs)
+{
+	Matrix obj(rhs.getRows(),rhs.getCols());
+    for(int r =0;r<rhs.getRows();r++)
+    {
+        for(int c=0;c<rhs.getCols();c++)
+        {
+            obj.matrix[r][c]=multipler*rhs.matrix[r][c];
+        }
+    }
+    return obj;
+}
