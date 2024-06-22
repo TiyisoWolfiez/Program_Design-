c9ff937485c45d23a47f8bf8d0769539a78a8dfb
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-06-22 22:35:12 +0200

    Update matrix.cpp

--- Code/Vectors/matrix.cpp
+++ Code/Vectors/matrix.cpp
@@ -14,3 +14,21 @@ Matrix operator*(const double& multipler, const Matrix& rhs)
     }
     return obj;
 }
+Matrix::Matrix(unsigned r, unsigned c)
+{
+	this->rows = r;
+	this->cols = c;
+	this->matrix=new double*[rows];
+	for(int F=0;F<this->rows;F++)
+	{
+		this->matrix[F]=new double [this->cols];
+	}
+	for(int P=0;P<this->rows;P++)
+	{
+		int D=0;
+		for(int x=0;x<rows;x++)
+		{
+			this->matrix[P][x]=0;
+		}
+	}
+}
