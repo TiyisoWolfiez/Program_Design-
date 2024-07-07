cec47c6b3f83d16847b6afa20782e7b2c91a1730
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-07-07 21:20:35 +0200

    Update Calculator.cpp

--- Code/Practical9/Calculator.cpp
+++ Code/Practical9/Calculator.cpp
@@ -7,3 +7,13 @@ Calculator<T>::Calculator(){
     this->valueStack = new Stack<T>();
     this->operatorStack = new Stack<Operator<T>*>();
 }
+
+template <typename T>
+Calculator<T>::~Calculator(){
+    if(!this->operatorStack->isEmpty()){
+        delete this->operatorStack;
+    }
+    if(!this->valueStack->isEmpty()){
+        delete this->valueStack;
+    }
+}
