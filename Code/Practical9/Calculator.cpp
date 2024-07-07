528ab7df53d9eab2c56165bf0ba9d190761e7437
tiyiso <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-07-07 21:20:18 +0200

    Update Calculator.cpp

--- Code/Practical9/Calculator.cpp
+++ Code/Practical9/Calculator.cpp
@@ -1,2 +1,9 @@
 #include "Calculator.h"
 #include <cstddef>
+
+
+template <typename T>
+Calculator<T>::Calculator(){
+    this->valueStack = new Stack<T>();
+    this->operatorStack = new Stack<Operator<T>*>();
+}
